74ef3217af63a62acd8d0e8ea548e05eec3a3ccc
Şahin Karahan <sahin.karahan@hotmail.com>
2024-12-10 10:51:49 +0300

    Initial Commit

---
+++ yugioh-1/yugioh_1App.swift
@@ -0,0 +1,17 @@
+//
+//  yugioh_1App.swift
+//  yugioh-1
+//
+//  Created by Şahin Karahan on 10.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct yugioh_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
